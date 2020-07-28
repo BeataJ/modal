fa7abe81ad4c31443217abb25c74e2b08d02e70a
beata <beata@emc2wave.com>
2020-07-28 16:14:04 -0700

    add event listeners on modalBtn, closeBtn

--- app.js
+++ app.js
@@ -6,3 +6,11 @@
 const modalBtn = document.querySelector('.modal-btn');
 const modalOverlay = document.querySelector('.modal-overlay');
 const colseBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click', () => {
+  modalOverlay.classList.add('open-modal');
+});
+
+colseBtn.addEventListener('click', () => {
+  modalOverlay.classList.remove('open-modal');
+});
